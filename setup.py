f17c6c516c95debccf1ddfbe8b0b6b8d240f7cd2
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-06-30 17:01:11 +0545

    added setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+import ez_setup
+
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+
+setup(
+    name="django-ryuutils",
+    version="0.1",
+    packages=find_packages(),
+    author="Utsav Gurmachhan Magar",
+    author_email="me@ryuuzu.xyz",
+    description="A package of utilities for Django (template tags, etc.)",
+    url="http://github.com/ryuuzu/django-utils/",
+    include_package_data=True,
+)
